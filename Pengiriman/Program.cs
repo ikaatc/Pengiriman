0e2c9e99505432cafa99d0bd82888128c17a00d6
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-04-05 10:12:03 +0700

    buat console writeline

--- Pengiriman/Program.cs
+++ Pengiriman/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Security.Cryptography;
+using MySql.data.mysqlclient;
 
 namespace Pengiriman
 {
@@ -10,6 +10,28 @@ namespace Pengiriman
     {
         static void Main(string[] args)
         {
+            Program pr = new Program();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi ke Database\n");
+                    Console.WriteLine("Masukkan Server : ");
+                    string server = Console.ReadLine();
+                    Console.WriteLine("Masukkan User ID : ");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan Password : ");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukkan database tujuan : ");
+                    string db = Console.ReadLine();
+                    Console.Write("\nKetik K untuk Terhubung ke Database : ");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                    switch (chr)
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
